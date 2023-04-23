7a3d588ea75ae5d0323640f414e83170ca210c1c
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-04-22 20:29:06 -0400

    clean code and add README.md

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -10,10 +10,6 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-//test to see if connection works
-// // the following assumes that you named your connection variable `pool`
-// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
-
 /// Users
 
 /**
@@ -21,16 +17,6 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithEmail = function (email) {
-//   let resolvedUser = null;
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user?.email.toLowerCase() === email?.toLowerCase()) {
-//       resolvedUser = user;
-//     }
-//   }
-//   return Promise.resolve(resolvedUser);
-// };
 
 const getUserWithEmail = (email) => {
 
@@ -64,12 +50,8 @@ const getUserWithEmail = (email) => {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithId = function (id) {
-//   return Promise.resolve(users[id]);
-// };
 
 const getUserWithId = (id) => {
-
   return pool
     .query(
       `SELECT id, name, email, password
@@ -100,12 +82,6 @@ const getUserWithId = (id) => {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-// const addUser = function (user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
 
 const addUser = (user) => {
 
@@ -139,9 +115,6 @@ const addUser = (user) => {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-// const getAllReservations = function (guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
 
 const getAllReservations = (guest_id, limit = 10) => {
 
@@ -173,13 +146,6 @@ const getAllReservations = (guest_id, limit = 10) => {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function (options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
 
 const getAllProperties = (options, limit = 10) => {
 
@@ -231,6 +197,7 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `AND cost_per_night <= $${queryParams.length} `;
   }
 
+  // 4
   if (options.minimum_rating) {
     queryParams.push(`${options.minimum_rating}`);
     // create a special if statement for rating as HAVING needs to be added after GROUP BY so the string is not the same as for the other params
@@ -245,7 +212,6 @@ const getAllProperties = (options, limit = 10) => {
   `;
   } else {
 
-  // 4
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
@@ -261,34 +227,11 @@ const getAllProperties = (options, limit = 10) => {
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
-  // return pool
-  //   .query(
-  //     `SELECT properties.*, avg(property_reviews.rating) as average_rating
-  //     FROM properties
-  //     LEFT JOIN property_reviews ON properties.id = property_id
-  //     GROUP BY properties.id
-  //     LIMIT $1`,
-  //     [limit])
-  //   .then((result) => {
-  //     console.log(result.rows);
-  //     return result.rows;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //   });
-// };
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function (property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
 
 const addProperty = (listing) => {
 
@@ -325,7 +268,6 @@ const addProperty = (listing) => {
       console.log(err.message);
     });
 };
-
 
 module.exports = {
   getUserWithEmail,
